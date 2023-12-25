f7958ab5dbffe0911603195c6bc9955a0942eaaa
Fariel Minozzo <farielandre@gmail.com>
2023-12-25 17:50:25 -0300

    inclusao menu burger

---
+++ main.js
@@ -0,0 +1,7 @@
+function clickHamburger() {
+  if (hamburger__items.style.display === "block") {
+    hamburger__items.style.display = "none";
+  } else {
+    hamburger__items.style.display = "block";
+  }
+}
