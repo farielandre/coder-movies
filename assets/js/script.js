00b183994018a271d0f2863b0f5092cbdfe15848
Fariel Minozzo <farielandre@gmail.com>
2023-12-27 13:35:16 -0300

    ajustes menu Mobile e responsividade pagina details

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function mobileMenu() {
+  if (mobile__items.style.display === "block") {
+    mobile__items.style.display = "none";
+  } else {
+    mobile__items.style.display = "block";
+  }
+}
--- main.js
+++
@@ -1,7 +0,0 @@
-function clickHamburger() {
-  if (hamburger__items.style.display === "block") {
-    hamburger__items.style.display = "none";
-  } else {
-    hamburger__items.style.display = "block";
-  }
-}
