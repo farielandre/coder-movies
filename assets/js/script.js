30675f83d4a7208e2da478fbe0f88221abec4abf
Fariel Minozzo <farielandre@gmail.com>
2024-01-14 23:21:56 -0300

    ajustes menu mobile e darkmode javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-function mobileMenu() {
-  if (mobile__items.style.display === "block") {
-    mobile__items.style.display = "none";
+function toggleColor() {
+  const html = document.documentElement;
+  if(html.classList.contains("lightmode")) { 
+    html.classList.remove("lightmode");
   } else {
-    mobile__items.style.display = "block";
+    html.classList.add("lightmode");
+  }
+}
+
+function mobileMenu() {
+  let menu = document.getElementById("mobile__items");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
   }
 }
