47aaa4d2d7bade4abb24dcd730cfeb73807eb6f8
Fariel Minozzo <farielandre@gmail.com>
2024-01-22 18:54:20 -0300

    Novo sistema de busca

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,26 @@ const TMDBKey = 'api_key=0d56374213cbb8fee874d62f13209cc4';
 const baseURL = 'https://api.themoviedb.org/3';
 const apiURL = baseURL + '/discover/movie?sort_by=popularity.desc&' + TMDBKey;
 const imgURL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = baseURL + '/search/movie?' + TMDBKey;
 
-fetch(apiURL)
+getMovieData(apiURL);
+
+function getMovieData(url) {
+  fetch(url)
     .then((response) => response.json())
     .then((data) => showMovies(data.results));
+}
 
 function showMovies(data) {
+  // console.log(data);
   data.forEach(movie => {
-    const {title, poster_path, vote_average} = movie;
+    const { title, poster_path, vote_average } = movie;
     const movieList = document.querySelector("div.movielist__container");
     let movielistCard = document.createElement("div");
     movielistCard.classList.add("movielist__card");
     movielistCard.innerHTML = `
         <a href="details.html">
-          <img src="${imgURL+poster_path}" alt="${title} movie poster"/>
+          <img src="${imgURL + poster_path}" alt="${title} movie poster"/>
         </a>
         <div class="movielist__card-info">
           <i class="fa-solid fa-heart fa-xs"></i>
@@ -28,51 +34,22 @@ function showMovies(data) {
   })
 }
 
-// API OMDB - Busca de filmes
+// API TMDB - Busca de filmes
 
 const searchForm = document.getElementById("header__form");
-const OMDBKey = "8e98fee1";
 
 searchForm.onsubmit = (ev) => {
   ev.preventDefault();
-  const searchMovies = ev.target.searchMovies.value;
+  const searchTitle = ev.target.searchTitle.value;
 
-  if (searchMovies == "") {
+  if (searchTitle == "") {
     alert("Type the movie's name");
     return;
   }
 
-  fetch(`https://www.omdbapi.com/?apikey=${OMDBKey}&s=${searchMovies}`)
-    .then((response) => response.json())
-    .then((json) => loadMovies(json));
-}
-
-// API OMDB - Resultado da busca de filmes
-
-const loadMovies = (json) => {
-  const movieList = document.querySelector("div.movielist__container");
-  movieList.innerHTML = "";
-
-  if (json.Response == "False") {
-    alert("Movie not found");
-    return;
+  if (searchTitle) {
+    getMovieData(searchURL + "&query=" + searchTitle);
   }
-
-  json.Search.forEach(element => {
-    let movielistCard = document.createElement("div");
-    movielistCard.classList.add("movielist__card");
-    movielistCard.innerHTML = `
-    <a href="details.html">
-      <img src="${element.Poster}" alt="${element.Title} movie poster"/>
-    </a>
-    <div class="movielist__card-info">
-      <i class="fa-solid fa-heart fa-xs"></i>
-      <span>9.1</span>
-    </div>
-    `
-    movieList.appendChild(movielistCard);
-  });
-
 }
 
 // Mudança de cor do site
